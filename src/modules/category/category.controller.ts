b8fa3603339e94c8966c7f4c81365664b9b1132c
Yusuf Abdullah <yusufabdullah20000@gmail.com>
2026-08-29 13:56:09 +0600

    updated category controller for admin

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -2,45 +2,46 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { categoryService } from "./category.service";
 import { sendResponse } from "../../utils/sendResponse";
-import httpStatus from 'http-status'
- const createCategories=catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-const payload=req.body
-const result =await categoryService.createCategoryIntoDb(payload)
-sendResponse(res, {
+import httpStatus from "http-status";
+
+const createCategories = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const payload = req.body;
+  const result = await categoryService.createCategoryIntoDb(payload);
+
+  sendResponse(res, {
     statusCode: httpStatus.CREATED,
     success: true,
     message: "Category created successfully",
     data: result,
   });
+});
 
-})
+const getAllCategories = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const result = await categoryService.getAllCategoriesFromDb();
 
-const getAllCategories=catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-    const result=await categoryService.getAllCategoriesFromDb()
-
-    sendResponse(res, {
-    statusCode: httpStatus.CREATED,
+  sendResponse(res, {
+    statusCode: httpStatus.OK, 
     success: true,
     message: "All Categories fetched successfully",
     data: result,
   });
-})
+});
 
 
-const getCategoryById=catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-    const {id}=req.params
-    const result=await categoryService.getCategoryByIdFromDb(id as string)
+const getCategoryById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const result = await categoryService.getCategoryByIdFromDb(id as string);
 
-    sendResponse(res, {
-    statusCode: httpStatus.CREATED,
+  sendResponse(res, {
+    statusCode: httpStatus.OK, 
     success: true,
     message: "Category fetched successfully",
     data: result,
   });
-})
+});
 
-export const categoryController={
-createCategories,
-getAllCategories,
-getCategoryById
-}
+export const categoryController = {
+  createCategories,
+  getAllCategories,
+  getCategoryById,
+};
